858d41cda1900fa3d879f506fdc63addf28d82d9
Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>
2024-06-27 09:07:46 -0300

    Visual Multi, change leader for prevent colision with neotree keybindings

--- lua/custom/plugins/vim_visual_multi.lua
+++ lua/custom/plugins/vim_visual_multi.lua
@@ -2,4 +2,10 @@
 -- Multi cursor Plugin :)
 --]]
 --
-return { 'mg979/vim-visual-multi' }
+return {
+  'mg979/vim-visual-multi',
+  lazy = false,
+  init = function()
+    vim.g.VM_leader = '_'
+  end,
+}

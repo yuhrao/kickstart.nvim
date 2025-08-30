d13f754bbd5d0944fdc5aca582c98253c2500349
Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>
2025-08-29 23:13:45 -0300

    fix clipboard

--- lua/custom/plugins/tmux-nvim.lua
+++ lua/custom/plugins/tmux-nvim.lua
@@ -3,6 +3,11 @@ return {
     'aserowy/tmux.nvim',
     config = function()
       return require('tmux').setup {
+        copy_sync = {
+          enable = true,
+          redirect_to_clipboard = true,  -- Redirect to system clipboard
+          sync_clipboard = false,  -- Don't override vim.g.clipboard
+        },
         resize = {
           enable_default_keybindings = false,
         },

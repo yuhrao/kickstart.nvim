f660e1f897d39e7471381f5c889618c7d27be4fe
orip <oriori1703@gmail.com>
2026-06-11 16:44:01 +0300

    Follow symlinks for the neovim config picker
    Closes: #2078

--- init.lua
+++ init.lua
@@ -581,7 +581,7 @@ do
   )
 
   -- Shortcut for searching your Neovim configuration files
-  vim.keymap.set('n', '<leader>sn', function() builtin.find_files { cwd = vim.fn.stdpath 'config' } end, { desc = '[S]earch [N]eovim files' })
+  vim.keymap.set('n', '<leader>sn', function() builtin.find_files { cwd = vim.fn.stdpath 'config', follow = true } end, { desc = '[S]earch [N]eovim files' })
 end
 
 -- ============================================================
